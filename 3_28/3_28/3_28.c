cf9f07fd1e6d0ccb0a34d96a1d7387db75c0ed0e
ZhT515 <77432977+ZhT515@users.noreply.github.com>
2021-03-28 10:29:11 +0800

    Add files via upload

---
+++ 3_28/3_28/3_28.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	FILE* pFile;
+	pFile = fopen("example.txt", "wb");
+	fputs("This is an apple.", pFile);
+	fseek(pFile, 9, SEEK_SET);
+	fputs(" sam", pFile);
+	fclose(pFile);
+	return 0;
+}
